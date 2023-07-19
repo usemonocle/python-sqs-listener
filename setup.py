6eae00b6e1c8f9a00eabe8bbef1b2264ec2132ce
matan <matan@usemonocle.com>
2023-07-19 13:21:16 +0300

    update requirement

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['aiobotocore[boto3]']
+    install_requires=['aioboto3']
 
 )
